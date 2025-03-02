3075aca2c1f13ed1b13a39a2fb3bb9fcd72db724
clapierre22 <clapierre@ithaca.edu>
2025-03-02 16:30:24 -0500

    Minor Item Entity file changes

--- src/main/java/clapierre/HealthItem.java
+++ src/main/java/clapierre/HealthItem.java
@@ -4,6 +4,7 @@ public class HealthItem extends Item{
     public HealthItem(int x, int y){
         super(x, y);
         this.health = 0;
+        this.entityName = "Health Entity";
     }
     
 //    Health Items: Common Baguette (10/100hp), Rare Donut (25/100hp), Epic Apple Pie (75/100hp), Legendary Tiramasu (100/100hp)
--- src/main/java/clapierre/ScoreItem.java
+++ src/main/java/clapierre/ScoreItem.java
@@ -3,6 +3,7 @@ package src.main.java.clapierre;
 public class ScoreItem extends Item {
     public ScoreItem(int x, int y){
         super(x, y);
+        this.entityName = "Score Entity";
     }
     
 //  Score Items: Common Loonie (10 score), Rare Toonie (20 score), Epic Green Tea (50 score), Legendary Harold (100 score)
