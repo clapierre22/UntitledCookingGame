713b17414c4569d315b55ad95b18b0b6af2e3de3
clapierre22 <clapierre@ithaca.edu>
2025-03-18 13:54:53 -0400

    Added EntityLogic.java, currently empty

---
+++ src/main/java/clapierre/EntityLogic.java
@@ -0,0 +1,9 @@
+package main.java.clapierre;
+
+public class EntityLogic {
+	Level currentLevel;
+	
+	public EntityLogic() {
+//		Depending on the entity type, controls the movement and logic for said entity
+	}
+}
